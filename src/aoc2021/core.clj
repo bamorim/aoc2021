35fdda0c4899edbd4ccc2face37d270a0d003e08
Bernardo Amorim <contato@bamorim.com>
2021-12-07 14:40:32 +0000

    naive solution for day 7

--- src/aoc2021/core.clj
+++ src/aoc2021/core.clj
@@ -7,9 +7,10 @@
    [aoc2021.day03 :as day03]
    [aoc2021.day04 :as day04]
    [aoc2021.day05 :as day05]
-   [aoc2021.day06 :as day06]))
+   [aoc2021.day06 :as day06]
+   [aoc2021.day07 :as day07]))
 
-(def default-day "6")
+(def default-day "7")
 
 (defn get-day [number]
   (case number
@@ -19,6 +20,7 @@
     04 [day04/parse day04/part-1 day04/part-2]
     05 [day05/parse day05/part-1 day05/part-2]
     06 [day06/parse day06/part-1 day06/part-2]
+    07 [day07/parse day07/part-1 day07/part-2]
     ((println "Invalid day!")
      (System/exit 1))))
 
---
+++ src/aoc2021/day07.clj
@@ -0,0 +1,21 @@
+(ns aoc2021.day07
+  (:require
+   [clojure.string :as str]))
+
+(defn parse [input]
+  (map read-string (str/split input #",")))
+
+(defn fuel-cost [cost-fn positions target]
+  (->>
+   positions
+   (map #(cost-fn (Math/abs (- %1 target))))
+   (reduce +)))
+
+(defn find-min-cost [cost-fn input]
+  (apply min 
+         (map 
+          #(fuel-cost cost-fn input %1)
+          (range (apply min input) (apply max input)))))
+
+(defn part-1 [input] (find-min-cost identity input))
+(defn part-2 [input] (find-min-cost #(/ (* %1 (+ %1 1)) 2) input))
